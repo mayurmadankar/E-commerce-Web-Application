a86a40f67bfd5ed76b39ac784e31865571285c6e
Mayur <madankarmayur5@gmail.com>
2025-06-17 17:52:10 +0530

    port

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ import { fileURLToPath } from "url";
 import path from "path";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
